0fcf9f9f617f119826aa19366eec508d7879a95d
jonathan santiago <jonathan_santiago_relva@hotmail.com>
2024-01-17 15:51:50 -0300

    adicionei sla em contato

--- batata.py
+++ batata.py
@@ -2,11 +2,13 @@ print("This is an empty header")
 print("This is an empty footer")
 
 
-def login(login,senha):
+def login(login, senha):
     print(login)
     print(senha)
     return True
 
-def contato(contato):
+
+def contato(contato, sla):
     print(contato)
+    print(sla)
     return True
