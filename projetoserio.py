505f6775f3f72e2126f49334c6ebc25a63149a72
ModalGR-000487 <caio-augusto@live.com>
2024-01-17 20:25:01 -0300

    fix: variavel distancia escrita errado

--- projetoserio.py
+++ projetoserio.py
@@ -4,5 +4,5 @@ x2, y2 = [float(x) for x in input().split()]
 
 distancia = math.sqrt((x2-x1)**2+(y2-y1)**2)
 
-print(f"{metragem:.4f}") 
-# Deixei um erro proposital!
+print(f"{distancia:.4f}") 
+# Corrigi o erro!!!
